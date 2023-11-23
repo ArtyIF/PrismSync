a1293908be3d26dd9680fce1b58614712ee9075f
ArtyIF <artyomisflash@mail.ru>
2023-11-23 22:52:28 +0300

    Optimized gallery

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
 
@@ -13,9 +11,9 @@ class GalleryPage extends StatefulWidget {
 }
 
 class _GalleryPageState extends State<GalleryPage> {
-  int _fetchingImagesState = 0;
+  bool _fetchingImagesStarted = false;
   bool _permissionChecked = false;
-  final List<Uint8List> _imageDatas = List.empty(growable: true);
+  final List<Widget> _imageWidgets = List.empty(growable: true);
 
   @override
   void initState() {
@@ -28,23 +26,27 @@ class _GalleryPageState extends State<GalleryPage> {
 
       if (!ps.hasAccess) {
         final snackBar = SnackBar(
-            content: const Text(
-                "Permission to access photos and videos on your device is required to use this app."),
-            action: SnackBarAction(
-                label: "Open Settings",
-                onPressed: () {
-                  PhotoManager.openSetting();
-                }));
+          content: const Text(
+              "Permission to access photos and videos on your device is required to use this app."),
+          action: SnackBarAction(
+            label: "Open Settings",
+            onPressed: () {
+              PhotoManager.openSetting();
+            },
+          ),
+        );
         messenger.showSnackBar(snackBar);
       } else if (!ps.isAuth) {
         final snackBar = SnackBar(
-            content: const Text(
-                "Permission to access all photos and videos on your device is recommended to use this app. For that, make sure the \"Photos and videos\" permission is set to \"Always allow all\"."),
-            action: SnackBarAction(
-                label: "Open Settings",
-                onPressed: () {
-                  PhotoManager.openSetting();
-                }));
+          content: const Text(
+              "Permission to access all photos and videos on your device is recommended to use this app. For that, make sure the \"Photos and videos\" permission is set to \"Always allow all\"."),
+          action: SnackBarAction(
+            label: "Open Settings",
+            onPressed: () {
+              PhotoManager.openSetting();
+            },
+          ),
+        );
         messenger.showSnackBar(snackBar);
       }
       _permissionChecked = true;
@@ -52,36 +54,42 @@ class _GalleryPageState extends State<GalleryPage> {
   }
 
   Future<void> _fetchImages(ScaffoldMessengerState messenger) async {
-    if (_fetchingImagesState > 1) return;
-    _fetchingImagesState = 2;
-
+    // TODO: caching
     for (int i = 0;
-        i < ((await PhotoManager.getAssetCount()) / 30).ceil();
+        i < ((await PhotoManager.getAssetCount()) / 60).ceil();
         i++) {
       var assets = await PhotoManager.getAssetListPaged(
-          page: i, pageCount: 30, type: RequestType.common);
-      List<Uint8List> imageData = List.empty(growable: true);
+          page: i, pageCount: 60, type: RequestType.common);
+      List<Widget> imageWidgets = List.empty(growable: true);
       for (var asset in assets) {
-        Uint8List? assetData =
-            await asset.thumbnailDataWithSize(const ThumbnailSize.square(256));
-        if (assetData != null) {
-          imageData.add(assetData);
-        }
+        Widget imagePreview = FutureBuilder(
+          future: asset.thumbnailDataWithSize(const ThumbnailSize.square(256)),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return FadeInImage(
+                placeholder: const AssetImage("images/loading.png"),
+                image: MemoryImage(snapshot.data!),
+                fit: BoxFit.cover,
+                fadeInDuration: Durations.short4,
+              );
+            }
+            return const Placeholder();
+          },
+        );
+        imageWidgets.add(imagePreview);
       }
 
       setState(() {
-        _imageDatas.addAll(imageData);
+        _imageWidgets.addAll(imageWidgets);
       });
     }
-    // TODO: https://stackoverflow.com/questions/68520264/simple-grid-gallery-with-photo-manager
-    // tldr on the todo: generate widgets right then and there
 
     return;
   }
 
   Future<void> _startFetchingImages(ScaffoldMessengerState messenger) async {
-    if (_fetchingImagesState > 0) return;
-    _fetchingImagesState = 1;
+    if (_fetchingImagesStarted) return;
+    _fetchingImagesStarted = true;
 
     await _checkPermission(messenger);
     if (!_permissionChecked) return;
@@ -99,16 +107,12 @@ class _GalleryPageState extends State<GalleryPage> {
         ),
         body: GridView.builder(
           gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 150, mainAxisSpacing: 5, crossAxisSpacing: 5),
-          itemBuilder: ((context, index) => FadeInImage(
-                placeholder: const AssetImage("images/loading.png"),
-                image: ScrollAwareImageProvider(
-                    context: DisposableBuildContext(this),
-                    imageProvider: MemoryImage(_imageDatas[index])),
-                fit: BoxFit.cover,
-                fadeInDuration: Durations.short4,
-              )),
-          itemCount: _imageDatas.length,
+            maxCrossAxisExtent: 150,
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+          ),
+          itemBuilder: (context, index) => _imageWidgets[index],
+          itemCount: _imageWidgets.length,
         ));
   }
 }
